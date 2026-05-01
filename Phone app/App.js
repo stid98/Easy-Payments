992b2352c6ec4eb06e09b801e239bd64be23140d
NDIKUMANA Aristide <ndikuari98@gmail.com>
2026-05-01 15:18:20 +0200

    Update App.js

--- Phone app/App.js
+++ Phone app/App.js
@@ -12,7 +12,7 @@ function displayMessage(){
     let amount = prompt('Enter amount to pay');
     let payee = prompt('Name of payee');
     let code = prompt('Enter code to pay to');
-    message = `Yello. Wishyuye ${amount} kuri ${payee}, ${code}. ikiguzi 0 RWF. Transaction ID 27604165584. Konti yawe ya mobile money usigaranye no 16255 RWF.`;
+    message = `Yello. Wishyuye ${amount} kuri ${payee}, ${code}. ikiguzi 0 RWF. Transaction ID 27644165584. Konti yawe ya mobile money usigaranye no 16255 RWF.`;
     messageOut.textContent = message;
     wrapper.style.display = 'flex';
 }
